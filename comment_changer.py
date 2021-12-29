4fc4baa12a41cd86e2de27609b9b666f3f3b3241
Iamlel <88410471+Iamlel@users.noreply.github.com>
2021-12-29 14:57:56 -0800

    Update comment_changer.py

--- comment_changer.py
+++ comment_changer.py
@@ -1,3 +1,5 @@
+import os
+
 with open("com.py", "w") as f:
   
   dif_same = input("Are you going to be using the same file? (true/false)\n")
@@ -9,9 +11,12 @@ with open("com.py", "w") as f:
   else:
     exit(f"Invalid input! ({dif_same})\n")
     
-  comment_char = input("Enter character used for comments in the file\n")
-  my_char = input("Enter the character you use for comments\n")
+  comment_char = input("Enter character you want\n")
+  my_char = input("Enter the character you used\n")
   
-  new_file = ("with open(f, 'r+') as fp:", f"\tfp.write('{comment_char}'.join((fp.read()).split('{my_char}'))")
-  
+  new_file = (f"fc = '{comment_char}'.join(((open(f, 'r')).read()).split('{my_char}'))", "(open(f, 'w')).write(fc)")
+
   f.write('\n'.join(new_file))
+
+if dif_same == "true":
+    os.system("python com.py")
